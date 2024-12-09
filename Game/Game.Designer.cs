50f49acfd8403e87b5c17275a7b3245c7753fbad
kiker <kiker@BARBOSIK>
2024-12-09 19:25:55 +0700

    Добавьте файлы проекта.

---
+++ Game/Game.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Game
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerGame = new System.Windows.Forms.Timer(this.components);
+            this.gamer = new System.Windows.Forms.PictureBox();
+            this.location = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gamer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.location)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TimerGame
+            // 
+            this.TimerGame.Tick += new System.EventHandler(this.TimerGame_Tick);
+            // 
+            // gamer
+            // 
+            this.gamer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.gamer.Location = new System.Drawing.Point(93, 314);
+            this.gamer.Name = "gamer";
+            this.gamer.Size = new System.Drawing.Size(100, 50);
+            this.gamer.TabIndex = 0;
+            this.gamer.TabStop = false;
+            // 
+            // location
+            // 
+            this.location.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.location.Location = new System.Drawing.Point(93, 168);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(100, 50);
+            this.location.TabIndex = 1;
+            this.location.TabStop = false;
+            this.location.Click += new System.EventHandler(this.location_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gamer);
+            this.Controls.Add(this.location);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Gamer_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Gamer_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.gamer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.location)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerGame;
+        private System.Windows.Forms.PictureBox gamer;
+        private System.Windows.Forms.PictureBox location;
+    }
+}
+
---
+++ Game/Game.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Game : Form
+    {
+        private const int CELL_SIZE_PIXELS = 30;                    // Размер клетки игрового поля, в пикселях
+        private const int ROWS_NUMBER = 10;                         // Количество рядов в игровом поле
+        private const int COLS_NUMBER = 10;                         // Количество столбцов в игровом поле
+        private const int FIELD_LEFT_OFFSET_PIXELS = 40;            // Отступ в пикселях от левого края формы
+        private const int FIELD_TOP_OFFSET_PIXELS = 15;             // Отступ в пикселях от правого края формы
+        private const int INITIAL_GAMER_SPEED_INTERVAL = 250;       // Задержка (свойство "Interval") для основного игрового таймера TimerGameLoop
+
+        private enum GamerDirection
+        {
+            Left,
+            Right,
+            Up,
+            Down
+        }
+
+        private GamerDirection gamerDirection = GamerDirection.Up;  // Текущее направление движения "Змейки"
+        //private PictureBox<Point> gamer = 
+        //private LinkedList<Point> gamer = new LinkedList<Point>();  // Список точек, содержащих координаты всего "тела Змейки"
+        private Random rand = new Random();                         // Генератор псевдослучайных чисел. нужен для генерации очередной "Еды" в произвольном месте игрового поля
+        private bool isGameEnded;                                   // Признак: игра завершена?
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void TimerGame_Tick(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    GameOver();
+                    break;
+                default:
+                    Invalidate();
+                    break;
+            }
+        }
+
+        private void TimerGame_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+            BackColor = Color.Black;
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            gamerDirection = GamerDirection.Up;
+            gamer.Location = new Point(150, 150);
+            isGameEnded = false;
+            TimerGame.Start();
+            TimerGame.Interval = INITIAL_GAMER_SPEED_INTERVAL;
+        }
+
+        private void GameOver()
+        {
+            isGameEnded = true;
+            TimerGame.Stop();
+            Close();
+        }
+
+        private bool IsGameOver()
+        {
+            switch (gamerDirection)
+            {
+                case GamerDirection.Left:
+                    return gamer.Location.X - CELL_SIZE_PIXELS < COLS_NUMBER + CELL_SIZE_PIXELS;
+                case GamerDirection.Right:
+                    return gamer.Location.X + CELL_SIZE_PIXELS >= COLS_NUMBER * CELL_SIZE_PIXELS + CELL_SIZE_PIXELS;
+                case GamerDirection.Down:
+                    return gamer.Location.Y + CELL_SIZE_PIXELS >= ROWS_NUMBER * CELL_SIZE_PIXELS;
+                case GamerDirection.Up:
+                    return gamer.Location.Y - CELL_SIZE_PIXELS < CELL_SIZE_PIXELS * 3;
+            }
+            return false;
+        }
+
+        private void MoveGamer()
+        {
+            switch (gamerDirection)
+            {
+                case GamerDirection.Up:
+                    if (IsGameOver())
+                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
+                    else
+                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y - CELL_SIZE_PIXELS);
+                    break;
+                case GamerDirection.Down:
+                    if (IsGameOver())
+                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
+                    else
+                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y + CELL_SIZE_PIXELS);
+                    break;
+                case GamerDirection.Right:
+                    if (IsGameOver())
+                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
+                    else
+                        gamer.Location = new Point(gamer.Location.X + CELL_SIZE_PIXELS, gamer.Location.Y);
+                    break;
+                case GamerDirection.Left:
+                    if (IsGameOver())
+                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
+                    else
+                        gamer.Location = new Point(gamer.Location.X - CELL_SIZE_PIXELS, gamer.Location.Y);
+                    break;
+            }
+        }
+
+        private void Gamer_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGrid();
+            DrawGamer();
+        }
+
+        private void DrawGrid()
+        {
+            location.Location = new Point(FIELD_LEFT_OFFSET_PIXELS, FIELD_TOP_OFFSET_PIXELS);
+            location.Image = Image.FromFile("Fon.png");
+            location.Width = CELL_SIZE_PIXELS * COLS_NUMBER;
+            location.Height = CELL_SIZE_PIXELS * ROWS_NUMBER;
+            
+        }
+
+        private void DrawGamer()
+        {
+            gamer.Image = Image.FromFile("Starlight.png");
+            gamer.Width = CELL_SIZE_PIXELS;
+            gamer.Height = CELL_SIZE_PIXELS;
+        }
+
+        private void Gamer_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                case Keys.A:
+                    gamerDirection = GamerDirection.Left;
+                    MoveGamer();
+                    break;
+                case Keys.Right:
+                case Keys.D:
+                    gamerDirection = GamerDirection.Right;
+                    MoveGamer();
+                    break;
+                case Keys.Down:
+                case Keys.S:
+                    gamerDirection = GamerDirection.Down;
+                    MoveGamer();
+                    break;
+                case Keys.Up:
+                case Keys.W:
+                    gamerDirection = GamerDirection.Up;
+                    MoveGamer();
+                    break;
+                case Keys.Escape:
+                    TimerGame.Stop();
+                    Close();
+                    break;
+                case Keys.Space:
+                    if (isGameEnded && !TimerGame.Enabled)
+                        StartGame();
+                    break;
+            }
+        }
+
+        private void location_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
