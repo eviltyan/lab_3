e7c56333a9aa1848ff6e75a9046a92634b514971
eviltyan <vlabuzova@gmail.com>
2024-12-13 14:02:59 +0700

    add scripts for scene

---
+++ Game/ISceneGenerate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    internal interface ISceneGenerate
+    {
+        void CreateLocation(PictureBox location, PictureBox gamer, int[] constant);
+    }
+}
---
+++ Game/SceneGenerate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    internal class SceneGenerate : ISceneGenerate
+    {
+        public void CreateLocation(PictureBox location, PictureBox gamer, int[] constant)
+        {
+            DrawGrid(location, constant);
+            DrawGamer(gamer, constant);
+        }
+
+        private void DrawGrid(PictureBox location, int[] constant)
+        {
+            location.Location = new Point(constant[3], constant[4]);
+            location.BackColor = Color.Coral;
+            location.Width = constant[0] * constant[2];
+            location.Height = constant[0] * constant[1];
+
+        }
+
+        private void DrawGamer(PictureBox gamer, int[] constant)
+        {
+            gamer.Image = Image.FromFile("Starlight.png");
+            gamer.Width = constant[0];
+            gamer.Height = constant[0];
+        }
+    }
+}
