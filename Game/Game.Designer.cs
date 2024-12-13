00d7b30a7c5bdcc2b70eeed487fcb2b1abb8f13f
Кристина <paramonova.kristina.2005@mail.ru>
2024-12-13 13:03:07 +0700

    change settings of scene

--- Game/Game.Designer.cs
+++ Game/Game.Designer.cs
@@ -28,17 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.TimerGame = new System.Windows.Forms.Timer(this.components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
             this.gamer = new System.Windows.Forms.PictureBox();
             this.location = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.drop = new System.Windows.Forms.Button();
+            this.inventory = new System.Windows.Forms.ListBox();
+            this.money = new System.Windows.Forms.PictureBox();
+            this.creeper = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.gamer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.location)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.money)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.creeper)).BeginInit();
             this.SuspendLayout();
-            // 
-            // TimerGame
-            // 
-            this.TimerGame.Tick += new System.EventHandler(this.TimerGame_Tick);
             // 
             // gamer
             // 
@@ -46,6 +49,7 @@
             this.gamer.Location = new System.Drawing.Point(93, 314);
             this.gamer.Name = "gamer";
             this.gamer.Size = new System.Drawing.Size(100, 50);
+            this.gamer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.gamer.TabIndex = 0;
             this.gamer.TabStop = false;
             // 
@@ -55,35 +59,97 @@
             this.location.Location = new System.Drawing.Point(93, 168);
             this.location.Name = "location";
             this.location.Size = new System.Drawing.Size(100, 50);
+            this.location.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.location.TabIndex = 1;
             this.location.TabStop = false;
-            this.location.Click += new System.EventHandler(this.location_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.drop);
+            this.panel1.Controls.Add(this.inventory);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(600, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // drop
+            // 
+            this.drop.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.drop.Location = new System.Drawing.Point(0, 390);
+            this.drop.Name = "drop";
+            this.drop.Size = new System.Drawing.Size(200, 60);
+            this.drop.TabIndex = 1;
+            this.drop.Text = "F - выбросить";
+            this.drop.UseVisualStyleBackColor = true;
+            this.drop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inventory
+            // 
+            this.inventory.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.inventory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inventory.FormattingEnabled = true;
+            this.inventory.ItemHeight = 16;
+            this.inventory.Location = new System.Drawing.Point(0, 0);
+            this.inventory.Name = "inventory";
+            this.inventory.Size = new System.Drawing.Size(200, 450);
+            this.inventory.TabIndex = 0;
+            // 
+            // money
+            // 
+            this.money.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.money.Image = ((System.Drawing.Image)(resources.GetObject("money.Image")));
+            this.money.Location = new System.Drawing.Point(348, 188);
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(30, 30);
+            this.money.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.money.TabIndex = 3;
+            this.money.TabStop = false;
+            // 
+            // creeper
+            // 
+            this.creeper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.creeper.Image = ((System.Drawing.Image)(resources.GetObject("creeper.Image")));
+            this.creeper.Location = new System.Drawing.Point(439, 188);
+            this.creeper.Name = "creeper";
+            this.creeper.Size = new System.Drawing.Size(30, 30);
+            this.creeper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.creeper.TabIndex = 4;
+            this.creeper.TabStop = false;
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.gamer);
+            this.Controls.Add(this.money);
+            this.Controls.Add(this.creeper);
             this.Controls.Add(this.location);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Game";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game";
-            this.Load += new System.EventHandler(this.Game_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Gamer_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Gamer_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.gamer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.location)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.money)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.creeper)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer TimerGame;
         private System.Windows.Forms.PictureBox gamer;
         private System.Windows.Forms.PictureBox location;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox inventory;
+        private System.Windows.Forms.PictureBox money;
+        private System.Windows.Forms.Button drop;
+        private System.Windows.Forms.PictureBox creeper;
     }
 }
 
--- Game/Game.cs
+++ Game/Game.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,179 +14,90 @@ namespace Game
     public partial class Game : Form
     {
         private const int CELL_SIZE_PIXELS = 30;                    // Размер клетки игрового поля, в пикселях
-        private const int ROWS_NUMBER = 10;                         // Количество рядов в игровом поле
-        private const int COLS_NUMBER = 10;                         // Количество столбцов в игровом поле
-        private const int FIELD_LEFT_OFFSET_PIXELS = 40;            // Отступ в пикселях от левого края формы
+        private const int ROWS_NUMBER = 11;                         // Количество рядов в игровом поле
+        private const int COLS_NUMBER = 13;                         // Количество столбцов в игровом поле
+        private const int FIELD_LEFT_OFFSET_PIXELS = 30;            // Отступ в пикселях от левого края формы
         private const int FIELD_TOP_OFFSET_PIXELS = 15;             // Отступ в пикселях от правого края формы
-        private const int INITIAL_GAMER_SPEED_INTERVAL = 250;       // Задержка (свойство "Interval") для основного игрового таймера TimerGameLoop
 
-        private enum GamerDirection
+        private ISceneGenerate sceneGenerate = new SceneGenerate();
+        private IGamer player = new Gamer();
+
+        private int[] ConstantValue()
         {
-            Left,
-            Right,
-            Up,
-            Down
+            int[] constant = new int[] { CELL_SIZE_PIXELS, ROWS_NUMBER, COLS_NUMBER, FIELD_LEFT_OFFSET_PIXELS, FIELD_TOP_OFFSET_PIXELS };
+            return constant;
         }
-
-        private GamerDirection gamerDirection = GamerDirection.Up;  // Текущее направление движения "Змейки"
-        //private PictureBox<Point> gamer = 
-        //private LinkedList<Point> gamer = new LinkedList<Point>();  // Список точек, содержащих координаты всего "тела Змейки"
-        private Random rand = new Random();                         // Генератор псевдослучайных чисел. нужен для генерации очередной "Еды" в произвольном месте игрового поля
-        private bool isGameEnded;                                   // Признак: игра завершена?
 
         public Game()
         {
             InitializeComponent();
-        }
 
-        private void TimerGame_Tick(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode)
-            {
-                case Keys.Escape:
-                    GameOver();
-                    break;
-                default:
-                    Invalidate();
-                    break;
-            }
-        }
+            this.KeyPreview = true;
+            this.KeyDown += Gamer_KeyDown;
 
-        private void TimerGame_Tick(object sender, EventArgs e)
-        {
-            Invalidate();
-        }
-
-        private void Game_Load(object sender, EventArgs e)
-        {
             DoubleBuffered = true;
             BackColor = Color.Black;
-            StartGame();
-        }
 
-        private void StartGame()
-        {
-            gamerDirection = GamerDirection.Up;
-            gamer.Location = new Point(150, 150);
-            isGameEnded = false;
-            TimerGame.Start();
-            TimerGame.Interval = INITIAL_GAMER_SPEED_INTERVAL;
-        }
-
-        private void GameOver()
-        {
-            isGameEnded = true;
-            TimerGame.Stop();
-            Close();
-        }
-
-        private bool IsGameOver()
-        {
-            switch (gamerDirection)
-            {
-                case GamerDirection.Left:
-                    return gamer.Location.X - CELL_SIZE_PIXELS < COLS_NUMBER + CELL_SIZE_PIXELS;
-                case GamerDirection.Right:
-                    return gamer.Location.X + CELL_SIZE_PIXELS >= COLS_NUMBER * CELL_SIZE_PIXELS + CELL_SIZE_PIXELS;
-                case GamerDirection.Down:
-                    return gamer.Location.Y + CELL_SIZE_PIXELS >= ROWS_NUMBER * CELL_SIZE_PIXELS;
-                case GamerDirection.Up:
-                    return gamer.Location.Y - CELL_SIZE_PIXELS < CELL_SIZE_PIXELS * 3;
-            }
-            return false;
-        }
-
-        private void MoveGamer()
-        {
-            switch (gamerDirection)
-            {
-                case GamerDirection.Up:
-                    if (IsGameOver())
-                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
-                    else
-                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y - CELL_SIZE_PIXELS);
-                    break;
-                case GamerDirection.Down:
-                    if (IsGameOver())
-                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
-                    else
-                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y + CELL_SIZE_PIXELS);
-                    break;
-                case GamerDirection.Right:
-                    if (IsGameOver())
-                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
-                    else
-                        gamer.Location = new Point(gamer.Location.X + CELL_SIZE_PIXELS, gamer.Location.Y);
-                    break;
-                case GamerDirection.Left:
-                    if (IsGameOver())
-                        gamer.Location = new Point(gamer.Location.X, gamer.Location.Y);
-                    else
-                        gamer.Location = new Point(gamer.Location.X - CELL_SIZE_PIXELS, gamer.Location.Y);
-                    break;
-            }
-        }
-
-        private void Gamer_Paint(object sender, PaintEventArgs e)
-        {
-            DrawGrid();
-            DrawGamer();
-        }
-
-        private void DrawGrid()
-        {
-            location.Location = new Point(FIELD_LEFT_OFFSET_PIXELS, FIELD_TOP_OFFSET_PIXELS);
-            location.Image = Image.FromFile("Fon.png");
-            location.Width = CELL_SIZE_PIXELS * COLS_NUMBER;
-            location.Height = CELL_SIZE_PIXELS * ROWS_NUMBER;
-            
-        }
-
-        private void DrawGamer()
-        {
-            gamer.Image = Image.FromFile("Starlight.png");
-            gamer.Width = CELL_SIZE_PIXELS;
-            gamer.Height = CELL_SIZE_PIXELS;
+            gamer.Location = new Point(210, 150);
+            money.Location = new Point(150, 150);
+            creeper.Location = new Point(150, 210);
+            sceneGenerate.CreateLocation(location, gamer, ConstantValue());
         }
 
         private void Gamer_KeyDown(object sender, KeyEventArgs e)
         {
+            int button;
             switch (e.KeyCode)
             {
-                case Keys.Left:
                 case Keys.A:
-                    gamerDirection = GamerDirection.Left;
-                    MoveGamer();
+                    button = 3;
+                    player.MoveDirection(button);
+                    player.MoveGamer(gamer, ConstantValue());
+                    InventoryPickUp();
                     break;
-                case Keys.Right:
+
                 case Keys.D:
-                    gamerDirection = GamerDirection.Right;
-                    MoveGamer();
+                    button = 2;
+                    player.MoveDirection(button);
+                    player.MoveGamer(gamer, ConstantValue());
+                    InventoryPickUp();
                     break;
-                case Keys.Down:
+
                 case Keys.S:
-                    gamerDirection = GamerDirection.Down;
-                    MoveGamer();
+                    button = 1;
+                    player.MoveDirection(button);
+                    player.MoveGamer(gamer, ConstantValue());
+                    InventoryPickUp();
                     break;
-                case Keys.Up:
+
                 case Keys.W:
-                    gamerDirection = GamerDirection.Up;
-                    MoveGamer();
+                    button = 0;
+                    player.MoveDirection(button);
+                    player.MoveGamer(gamer, ConstantValue());
+                    InventoryPickUp();
                     break;
+
+                case Keys.F:
+                    player.GetInventory().Drop(inventory, money, gamer);
+                    player.GetInventory().Drop(inventory, creeper, gamer);
+                    break;
+
                 case Keys.Escape:
-                    TimerGame.Stop();
                     Close();
-                    break;
-                case Keys.Space:
-                    if (isGameEnded && !TimerGame.Enabled)
-                        StartGame();
                     break;
             }
         }
 
-        private void location_Click(object sender, EventArgs e)
+        private void InventoryPickUp()
         {
+            player.GetInventory().PickUp(inventory, money, gamer);
+            player.GetInventory().PickUp(inventory, creeper, gamer);
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.GetInventory().Drop(inventory, money, gamer);
+            player.GetInventory().Drop(inventory, creeper, gamer);
         }
     }
 }
